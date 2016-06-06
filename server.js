1490fceb929ad85d453f4533dacac0688a54054c
Anishnirmal <anishnirmal.d@gmail.com>
2016-06-06 16:42:12 +0530

    body parser

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.set('port', (process.env.PORT || 5000))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
 });
