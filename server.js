49b0029b276d3e6a155830649d22035ddb583562
Anishnirmal <anishnirmal.d@gmail.com>
2016-06-06 15:37:26 +0530

    port changes

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ var request = require('request');
 var app = express();
 var scores = require('scores-parser');  
 
-app.set('port', (process.env.PORT || 8081))
+app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
+});
+
+// for facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'this_is_my_bot_and_my_name_is_my_pwd') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
 });
 
 app.get('/getproducts', function (req, resp){
