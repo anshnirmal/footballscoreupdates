84218c513a075cde6b8ae97473ab93ced8ca9a2a
Anishnirmal <anishnirmal.d@gmail.com>
2016-06-06 15:57:58 +0530

    test commit bot

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 var scores = require('scores-parser');  
+var token = "EAAXPLuYkUpEBAEJq4kG9LhlnGFY6RLmjXeR1Ia6F2qTCigMl4CwgoEeNra3s1aWAYK9A7qZBYE5NYUTbZBZCnv2WGYeHO8ASMgGQs4A1lnF3hKzflVy2Q80RV4gIdWhPukDsfhLJhZCapMDX3hoGe9ilfoA0oZB4Yhjodve5oe8cMS1EPIdZAk";
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -17,6 +18,53 @@ app.get('/webhook/', function (req, res) {
 	}
 	res.send('Error, wrong token')
 });
+
+// to post data
+app.post('/webhook/', function (req, res) {
+	messaging_events = req.body.entry[0].messaging
+	for (i = 0; i < messaging_events.length; i++) {
+		event = req.body.entry[0].messaging[i]
+		sender = event.sender.id
+		if (event.message && event.message.text) {
+			prop = event.message.text
+			console.log("#################", prop);
+			console.log("#################", sender);
+      // GetMovieName(sender,text)
+      IsValidProperty(sender,prop);
+		}
+	}
+	res.sendStatus(200)
+})
+
+function IsValidProperty(sender, prop){
+  SendInfoToUser(prop, sender);
+}
+
+function SendInfoToUser(prop, sender){    
+    PostToUser(sender, 'anish');
+}
+
+function PostToUser(senderId, message){
+  console.log(message);
+	messageData = {
+		text:message
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:senderId},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
 
 app.get('/getproducts', function (req, resp){
   scores({ date: '2016-06-05' }, function (data) {
