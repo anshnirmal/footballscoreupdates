4d6ddde72d49a0393275c92fa76bb5f18f5dd697
Anishnirmal <anishnirmal.d@gmail.com>
2016-06-10 13:53:45 +0530

    post to user changes

--- server.js
+++ server.js
@@ -54,20 +54,20 @@ climate.getMaximumTemp( '' + prop, function(temp){
 		    
 });
 
-//climate.getMinimumTemp('' + prop, function(temp){
-//	message += "Minimum temperature: " + temp + '\n';
-//    console.log("Minimum temperature: " + temp);
-//});
-// 
-//climate.getActualTemp('' + prop, function(temp){
-//	message += "Actual temperature: " + temp + '\n';
-//    console.log("Actual temperature: " + temp);
-//});
-// 
-//climate.getWindSpeed('' + prop, function(speed){
-//	message += "Wind speed: " + temp + '\n';
-//    console.log("Wind speed: " + speed);
-//});
+climate.getMinimumTemp('' + prop, function(temp){
+	message += "Minimum temperature: " + temp + '\n';
+    console.log("Minimum temperature: " + temp);
+});
+ 
+climate.getActualTemp('' + prop, function(temp){
+	message += "Actual temperature: " + temp + '\n';
+    console.log("Actual temperature: " + temp);
+});
+ 
+climate.getWindSpeed('' + prop, function(speed){
+	message += "Wind speed: " + speed + '\n';
+    console.log("Wind speed: " + speed);
+});
 
 climate.getClimateDescription('' + prop, function(description){
 	message += "Climate description: " + description + '\n';
